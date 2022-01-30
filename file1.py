38b2d1ac5dff6d50b03790ea48c40d4f6ca104e4
Shubh6180 <90522593+Shubh6180@users.noreply.github.com>
2022-01-31 01:59:45 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,6 @@
+file = open("file1.txt")
+lines = file.readlines()
+for i in lines:
+    print(i)
+
+    
---
+++ file2.py
@@ -0,0 +1,5 @@
+file = open("file2.txt")
+lines = file.readlines()
+
+for i in lines:
+    print(i)
